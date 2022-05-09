fac3697c4edc78198022a2f5377f95421a7f4dbb
gwengardner <glg@sas.upenn.edu>
2022-05-09 14:20:52 +0200

    added ranges to squark

--- example/squark.py
+++ example/squark.py
@@ -13,16 +13,14 @@ filename = 'squark'
 
 vals = {
         '8 squarks': {
-                    'HL-LHC': [2.5, 'CR extrapolation from CMS Run 2'],
-                    'HL-LHC': [2.6, 'CR extrapolation from ATLAS Run 2'],
-                    'FCC-hh': [14.4, 'CR extrapolation from CMS Run 2'],
+                    'HL-LHC': [(2.5, 2.55, 2.6), 'CR extrapolation from Run 2'],
+                    'FCC-hh': [(10,(14.4+15.1)/2,15.1), 'CR extrapolation from Run 2'],
                     'annotation': r'$m(\chi) = 0$',
                     'current limits': 1.66,
         },
         '1 squark': {
-                    'HL-LHC': [1.8, 'CR extrapolation from CMS Run 2'],
-                    'HL-LHC': [1.9, 'CR extrapolation from ATLAS Run 2'],
-                    'FCC-hh': [10, 'CR extrapolation from CMS Run 2'],
+                    'HL-LHC': [(1.8, 1.85, 1.9), 'CR extrapolation from Run 2'],
+                    'FCC-hh': [(10,10.2,10.4), 'CR extrapolation from Run 2'],
                     'annotation': r'$m(\chi)=0$',
                     'current limits': 1.1,
         },
